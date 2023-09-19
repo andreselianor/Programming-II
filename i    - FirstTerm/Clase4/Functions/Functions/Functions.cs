e81ab239790e46952ccabc2e46c9eb867279adac
andreselianor <113841548+andreselianor@users.noreply.github.com>
2023-09-19 19:57:57 +0200

    martes

---
+++ i    - FirstTerm/Clase4/Functions/Functions/Functions.cs
@@ -0,0 +1,29 @@
+﻿namespace Functions
+{
+    public class Functions
+    {
+        
+        public static int GetSumatory1(int number)
+        {
+            int result = 0;
+            int i = 1;
+
+            while(i <= number)
+            {
+                result += i;
+                i++;
+            }
+            return result;
+        }
+
+        public static int GetSumatory2(int number)
+        {
+            int result = 0;
+            for(int i = 1; i <= number; i++)
+            {
+                result += i;
+            }
+            return result;
+        }
+    }
+}
---
+++ i    - FirstTerm/Clase4/Functions/Functions/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Funciones");
+
+            int result = Functions.GetSumatory1(5);
+            int result2 = Functions.GetSumatory2(5);
+            Console.WriteLine(result);
+            Console.WriteLine(result2);
+        }
+    }
+}
