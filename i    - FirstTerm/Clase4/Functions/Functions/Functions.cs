90a3b4e259ae678d8e08397c773e8f1cd71d6383
andreselianor <113841548+andreselianor@users.noreply.github.com>
2023-09-21 08:47:56 +0200

    Clase del Miercoles

--- i    - FirstTerm/Clase4/Functions/Functions/Functions.cs
+++ i    - FirstTerm/Clase4/Functions/Functions/Functions.cs
@@ -1,8 +1,7 @@
 ﻿namespace Functions
 {
     public class Functions
-    {
-        
+    {        
         public static int GetSumatory1(int number)
         {
             int result = 0;
@@ -24,6 +23,54 @@
                 result += i;
             }
             return result;
+        }
+
+        public static bool IsEven(int number)
+        {
+            if (number % 2 == 0)
+                return true;
+            return false;
+        }
+
+        public static void GetSequence(int number)
+        {
+            int count = 0;
+            int result = 0;
+            int resultCounter = 0;
+
+            while(resultCounter < number)
+            {
+                if (IsEven(count))
+                {
+                    result = count * 2 * (1);
+                }
+                else
+                {
+                    result = count * 2 * (-1);
+                }
+                Console.WriteLine(result);
+                resultCounter = Math.Abs(result);
+                count++;
+            }
+        }
+
+        public static void GetSequence2(int number)
+        {
+            int result = 0;
+
+            for(int i = 1; i <= number/2; ++i)
+            {
+                if (IsEven(i))
+                {
+                    result = i * 2 * (1);
+                }
+                else
+                {
+                    result = i * 2 * (-1);
+                }
+
+                Console.WriteLine(result);
+            }
         }
     }
 }
--- i    - FirstTerm/Clase4/Functions/Functions/Program.cs
+++ i    - FirstTerm/Clase4/Functions/Functions/Program.cs
@@ -6,10 +6,14 @@
         {
             Console.WriteLine("Funciones");
 
+            /*
             int result = Functions.GetSumatory1(5);
             int result2 = Functions.GetSumatory2(5);
             Console.WriteLine(result);
             Console.WriteLine(result2);
+            */
+
+            Functions.GetSequence2(10);
         }
     }
 }
