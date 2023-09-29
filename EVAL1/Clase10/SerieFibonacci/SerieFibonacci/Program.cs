d8dcb21b994ea6eed0f24ef793b5e95dc3179923
andreselianor <113841548+andreselianor@users.noreply.github.com>
2023-09-29 18:43:41 +0200

    clase Viernes 29

---
+++ EVAL1/Clase10/SerieFibonacci/SerieFibonacci/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SerieFibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Serie de Fibonacci");
+            string Fibonacci = Function.Fibonacci(10);
+            Console.WriteLine(Fibonacci);
+
+            string Fibonacci2 = Function.Fibonacci2(10);
+            Console.WriteLine(Fibonacci2);
+        }
+    }
+}
