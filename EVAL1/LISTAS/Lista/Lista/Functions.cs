fa608962f5e4c04208126d2f1336975b26f0616f
andreselianor <113841548+andreselianor@users.noreply.github.com>
2023-10-02 16:53:17 +0200

    clase 10.02

---
+++ EVAL1/LISTAS/Lista/Lista/Functions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista
+{
+    public class Functions
+    {
+        public static int GetGreaterThanZero(List<double> list)
+        {
+            int result = 0;
+
+            for(int i = 0; i < list.Count; i++)
+            {
+                if (list[i] > 0)
+                    result++;
+            }
+            return result;
+        }
+
+        public static int GetGreaterThanZero(double[] list)
+        {
+            int result = 0;
+
+            for (int i = 0; i < list.Length; i++)
+            {
+                if (list[i] > 0)
+                    result++;
+            }
+            return result;
+        }
+    }
+}
