44b8c0c7bf197c9395d6d72c976323667b564ec7
andreselianor <113841548+andreselianor@users.noreply.github.com>
2023-10-16 16:52:03 +0200

    clase Lunes 16

--- EVAL1/OBJETOS/Dolphin/Dolphin01/Program.cs
+++ EVAL1/OBJETOS/Dolphin/Dolphin01/Program.cs
@@ -5,6 +5,26 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Echo the Dolphin");
+
+            Character c1;
+            Character c2;
+            c1 = new Character();
+            c2 = c1;
+
+            c1.name = "VanDamme";
+            c1.Type = CharacterType.POLICE;
+
+            List<Character> list;
+            list = new List<Character>();
+
+            list.Add(c1);
+            list.Add(c2);
+
+
+
+
+
+
         }
     }
 }
