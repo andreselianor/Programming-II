531bf341600201e7364960a08930859f19c42365
andreselianor <113841548+andreselianor@users.noreply.github.com>
2023-10-17 19:57:54 +0200

    clase martes

---
+++ EVAL1/GRAFICO/ProyectoGrafico/ProyectoGrafico/Utils.cs
@@ -0,0 +1,17 @@
+﻿namespace ProyectoGrafico
+{
+    public class Utils
+    {
+        private static Random random = new Random();
+
+        public static double GetRandom()
+        {
+            return random.NextDouble();
+        }
+
+        public static double GetRandomDouble(double min, double max)
+        {
+            return (min + (random.NextDouble() * (max - min)));
+        }
+    }
+}
