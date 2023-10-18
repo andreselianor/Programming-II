792e2c87c5279fee59b68559efb7ff18a1f70684
andreselianor <113841548+andreselianor@users.noreply.github.com>
2023-10-18 05:57:38 +0200

    miercoles

---
+++ PersonalFolder/ii   - Investigations/Dibujos/Grafico1/Grafico1/Program.cs
@@ -0,0 +1,40 @@
+﻿using UDK;
+
+namespace Grafico1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyGame game = new MyGame();
+            UDK.Game.Launch(game);
+        }
+    }
+
+    public class MyGame : UDK.IGameDelegate
+    {
+        public void OnAnimate(GameDelegateEvent gameEvent)
+        {
+        }
+
+        public void OnDraw(GameDelegateEvent gameEvent, ICanvas canvas)
+        {
+            canvas.Clear(0.0, 0.0, 0.0, 1.0);
+            canvas.Camera.SetRectangle(0, 0, 10, 10);
+            canvas.FillShader.SetColor(1.0, 0.0, 0.0, 1.0);
+            canvas.DrawRectangle(1, 1, 2, 2);
+        }
+
+        public void OnKeyboard(GameDelegateEvent gameEvent, IKeyboard keyboard, IMouse mouse)
+        {
+        }
+
+        public void OnLoad(GameDelegateEvent gameEvent)
+        {
+        }
+
+        public void OnUnload(GameDelegateEvent gameEvent)
+        {
+        }
+    }
+}
