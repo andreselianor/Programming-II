2ee7d02d6d52f7326162b85796632522d40d672f
andreselianor <113841548+andreselianor@users.noreply.github.com>
2023-10-31 19:59:52 +0100

    clase Martes 30 Octubre

---
+++ PROG/EV1/Cards/Cards/Card.cs
@@ -0,0 +1,83 @@
+﻿namespace Cards
+{
+    public enum Palo
+    {
+        PICA,
+        TREBOL,
+        CORAZON,
+        DIAMANTE
+    }
+
+    public enum TypeCard
+    {
+        COMMON,
+        AS,
+        JACK,
+        QUEEN,
+        KING
+    }
+
+    public enum TypeColor
+    {
+        RED,
+        BLACK
+    }
+
+    public class Card
+    {
+        private int _number;
+        private Palo _palo;
+
+        public Card(int number, Palo palo)
+        {
+            _number = number;
+            _palo = palo;
+        }
+
+        public int GetNumber()
+        {
+            return _number;
+        }        
+
+        public Palo GetPalo()
+        {
+            return _palo;
+        }
+
+        public bool IsValid()
+        {
+            if (_number < 0)
+                return false;
+            else if (_number > 11)
+                return false;
+            return true;
+        }
+
+        public TypeColor GetColor()
+        {
+            if(_palo == Palo.PICA || _palo == Palo.CORAZON)
+                return TypeColor.RED;
+            else
+                return TypeColor.BLACK;
+        }
+
+        public bool IsFigure()
+        {
+            return (_number > 7 || _number < 11);
+        }
+
+        public TypeCard GetFigureType()
+        {
+            if (_number == 1)
+                return TypeCard.AS;
+            if (_number == 8)
+                return TypeCard.JACK;
+            if (_number == 9)
+                return TypeCard.QUEEN;
+            if (_number == 10)
+                return TypeCard.KING;
+
+            return TypeCard.COMMON;
+        }
+    }
+}
---
+++ PROG/EV1/Cards/Cards/CardUtils.cs
@@ -0,0 +1,29 @@
+﻿namespace Cards
+{
+    public class CardUtils
+    {
+        public static int GetBlackJackValue(List<Card> listCards)
+        {
+            int result = 0;
+
+            foreach(Card card in listCards)
+            {
+                result += CalculateValueCard(card);
+            }
+
+            return result;
+        }
+
+        public static int CalculateValueCard(Card card)
+        {
+            if (card.GetFigureType() == TypeCard.AS)
+                return 1;
+            if (card.GetFigureType() == TypeCard.JACK ||
+                card.GetFigureType() == TypeCard.QUEEN ||
+                card.GetFigureType() == TypeCard.KING)
+                return 10;
+            else
+                return card.GetNumber();
+        }
+    }
+}
