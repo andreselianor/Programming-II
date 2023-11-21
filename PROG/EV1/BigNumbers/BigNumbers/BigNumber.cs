390b98fe9ddaf5b7b15e77f774e4578f5bb4ae6e
andreselianor <113841548+andreselianor@users.noreply.github.com>
2023-11-21 17:13:38 +0100

    bigNumber martes

--- PROG/EV1/BigNumbers/BigNumbers/BigNumber.cs
+++ PROG/EV1/BigNumbers/BigNumbers/BigNumber.cs
@@ -5,9 +5,6 @@ namespace BigNumbers
     public class BigNumber
     {
         private List<int> _list = new List<int>();
-        private int _restNumber;
-
-
 
         #region Constructores
         public BigNumber(long bigNumber)
@@ -31,19 +28,16 @@ namespace BigNumbers
         #region Set BigNumber
         public void Set(long bigNumber)
         {
-            long nextBigNumber;
-            int numberList;
+            int digit;
 
             while (bigNumber > 0)
             {
-                numberList = (int)bigNumber % 10;
-                nextBigNumber = (int)bigNumber / 10;
+                digit = (int)bigNumber % 10;
+                bigNumber = (int)bigNumber / 10;
 
-                _list.Add(numberList);
-                bigNumber = nextBigNumber;
+                _list.Add(digit);
             }
         }
-
 
         public void Set(string bigNumberString)
         {
@@ -53,26 +47,38 @@ namespace BigNumbers
                 bigNumberCount++;
             }
 
-            for (int i = bigNumberCount - 1; i == 0; i--)
+            for (int i = bigNumberCount - 1; i >= 0; i--)
             {
                 _list.Add(bigNumberString[i]);
             }
         }
+        #endregion
 
+
+        #region GetDigits
+        public int GetDigitCount()
+        {
+            return _list.Count;
+        }
+
+        public int GetDigitAt(int index)
+        {
+            return _list[index];
+        }
 
         public List<int> GetBigNumber()
         {
             return _list;
         }
 
-
         override public string? ToString()
         {
             return _list.ToString();
         }
+        #endregion
 
 
-        // Funciones CLONE
+        #region Clone
         public BigNumber Clone()
         {
             return this;
@@ -91,27 +97,12 @@ namespace BigNumbers
         }
         #endregion
 
-
-
-        #region GetDigits
-        public int GetDigitCount()
-        {
-            return _list.Count;
-        }
-
-        public int GetDigitAt(int index)
-        {
-            return _list[index];
-        }
-        #endregion
-
-
-        #region Utils
+        #region Display
         public void ShowBigNumber()
         {
             foreach (int number in _list)
             {
-                Console.Write(number + " ");
+                Console.Write(number);
             }
             Console.WriteLine("\n");
         }
@@ -121,17 +112,80 @@ namespace BigNumbers
 
             for (int i = _list.Count - 1; i >= 0; i--)
             {
-                Console.Write(_list[i] + " ");
+                Console.Write(_list[i]);
             }
 
             Console.WriteLine("\n");
         }
-
         #endregion
 
-        #region Funciones Calculate
+        #region Calculate
+        public static BigNumber Sum(BigNumber bigNumber1, BigNumber bigNumber2)
+        {
+            BigNumber bigNumberResult = new BigNumber();
+
+            int Count1 = bigNumber1.GetDigitCount();
+            int Count2 = bigNumber2.GetDigitCount();
+
+            int numberDigitsBigNumber = GetMaxCountBigNumber(Count1, Count2);
+
+            int operatorSum1;
+            int operatorSum2;
+            int operatorRest = 0;
+            int result;
+
+            for (int i = 0; i < numberDigitsBigNumber; i++)
+            {
+                if (i > Count1)
+                    operatorSum1 = 0;
+                else
+                    operatorSum1 = bigNumber1._list[i];
+
+                if (i > Count2)
+                    operatorSum2 = 0;
+                else
+                    operatorSum2 = bigNumber2._list[i];
 
 
+                result = MakeSum(operatorSum1, operatorSum2, operatorRest);
+
+                if (result > 9)
+                {
+                    operatorRest = GetRest(result);
+                    result -= 10;
+                }
+
+                bigNumberResult._list.Add(result);
+            }
+            if (operatorRest > 0)
+                bigNumberResult._list.Add(operatorRest);
+
+            return bigNumberResult;
+        }
+
+        public static int GetMaxCountBigNumber(int Count1, int Count2)
+        {
+            return Count1 > Count2 ? Count1 : Count2;
+        }
+
+        public static int MakeSum(int operator1, int operator2, int operatorRest)
+        {
+            return operator1 + operator2 + operatorRest;
+        }
+
+        public static int GetRest(int number)
+        {
+            int count = 0;
+
+            while (number >= 9)
+            {
+                number -= 10;
+                count++;
+            }
+            return count;
+        }
+
+        /*
         public static BigNumber Sum(BigNumber bigNumber1, BigNumber bigNumber2)
         {
             BigNumber bigNumberResult = new BigNumber();
@@ -187,16 +241,7 @@ namespace BigNumbers
 
             return longitud1 > longitud2 ? longitud1 : longitud2;
         }
+        */
         #endregion
-
-
-
-
-
-
-
-
-
-
     }
 }
--- PROG/EV1/BigNumbers/BigNumbers/Program.cs
+++ PROG/EV1/BigNumbers/BigNumbers/Program.cs
@@ -4,11 +4,15 @@
     {
         static void Main(string[] args)
         {
-            BigNumber test1 = new BigNumber(125);
-            BigNumber test2 = new BigNumber(875);
-            BigNumber result;
-            result = BigNumber.Sum(test1, test2);
+            BigNumber test1 = new BigNumber(115000);
+            BigNumber test2 = new BigNumber(332500);
+            
+            test1.ShowBigNumberReverse();
+            test2.ShowBigNumberReverse();
+            Console.WriteLine("***");
 
+            BigNumber result = BigNumber.Sum(test1, test2);
+            result.ShowBigNumber();
             result.ShowBigNumberReverse();
         }
     }
