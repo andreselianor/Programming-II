c05cd9375a1bfdd775831a487e070e5915a7dcb8
andreselianor <113841548+andreselianor@users.noreply.github.com>
2023-11-27 12:25:29 +0100

    fin de semana casa

---
+++ PersonalFolder/ii   - Investigations/FiltrosGraficos/FiltrosGraficos/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace FiltrosGraficos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path1 = "..\\..\\..\\..\\resources\\okeeffe.png";
+            string path2 = "..\\..\\..\\..\\resources\\okeeffeMOD.png";
+
+            Filtros.Filter(path1, path2);
+        }
+    }
+}
