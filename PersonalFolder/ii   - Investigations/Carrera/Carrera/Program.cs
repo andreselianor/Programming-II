199a8598db7c2afa8852090b55adb6e9b77aa950
andreselianor <113841548+andreselianor@users.noreply.github.com>
2023-11-29 20:07:10 +0100

    ejercicio prueba lunes

---
+++ PersonalFolder/ii   - Investigations/Carrera/Carrera/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Carrera
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Race raceFirst = new Race();
+            raceFirst.CreateRace(5, 20.0);
+            while (!raceFirst.IsWinner())
+            {
+                raceFirst.Simulate();
+                foreach(Runner r in raceFirst.RunnersList)
+                {
+                    Console.WriteLine(r.Position);                    
+                }
+                Console.ReadLine();
+            }
+
+            Runner winner = new Runner();
+            winner = raceFirst.GetWinner();
+
+        }
+    }
+}
---
+++ PersonalFolder/ii   - Investigations/Carrera/Carrera/Runner.cs
@@ -0,0 +1,43 @@
+﻿namespace Carrera
+{
+    public enum Team
+    {
+        TEAMA,
+        TEAMB,
+        TEAMC
+    }
+    public class Runner
+    {
+        private string _name = "";
+        private double _velocity;
+        private double _position;
+        private Team _team;
+        private int _id;
+
+
+        public Runner()
+        {
+            _velocity = Utils.GetRandom(0, 10);
+        }
+
+        public Runner(string name, double position, Team team, int id)
+        {
+            _name = name;
+            _position = position;
+            _team = team;
+            _id = id;
+        }
+
+        public double Position => _position;
+        public string Name => _name;
+        public Team Team => _team;
+
+        public void Move()
+        {
+            double runnerDistance = _velocity * Utils.GetRandom();
+            _position += runnerDistance;
+        }
+
+
+    }
+}
