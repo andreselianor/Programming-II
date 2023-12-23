ea9035439f44865a7d5d4ed9c2092e37a875cdfc
andreselianor <113841548+andreselianor@users.noreply.github.com>
2023-12-23 08:34:12 +0100

    sabado 23 DIC

---
+++ PersonalFolder/ii   - Investigations/Ejemplos Guiados grafico C#/UDK_Test_Release/samples/SampleCanvas_ImageDatabase.cs
@@ -0,0 +1,43 @@
+﻿using UDK;
+
+namespace UDK_Test
+{
+    public class SampleCanvas_ImageDatabase : IGameDelegate
+    {
+        ImageDatabase? database;
+        double x = 0.0f;
+        double y = 0.0f;
+
+        public void OnDraw(GameDelegateEvent gameEvent, ICanvas canvas)
+        {
+            canvas.Clear(new rgba_f64(0.2, 0.3, 0.3, 1.0));
+            canvas.Camera.SetRect(rect2d_f64.FromMinMax(-2.0, -2.0, 10.0, 2.0), true);
+
+            var image = database.LoadImage("resources/map/world.png");
+            canvas.FillShader.SetImage(image, new rect2d_f64(0.5, 0.5, 0.5, 0.5), new rgba_f64(1, 1, 1, 1));
+            canvas.Transform.SetTranslation(x, y - 1.0f);
+            canvas.Mask.PushCircle(new rect2d_f64(0.2, 0.2, 0.8, 0.8));
+            canvas.DrawRectangle(new rect2d_f64(0, 0, 1, 1));
+            canvas.Mask.Pop();
+        }
+
+        public void OnKeyboard(GameDelegateEvent gameEvent, IKeyboard keyboard, IMouse mouse)
+        {
+
+        }
+
+        public void OnLoad(GameDelegateEvent gameEvent)
+        {
+            database = new ImageDatabase(gameEvent.canvasContext);
+            database.LoadImage("resources/map/world.png");
+        }
+
+        public void OnUnload(GameDelegateEvent gameEvent)
+        {
+        }
+
+        public void OnAnimate(GameDelegateEvent gameEvent)
+        {
+        }
+    }
+}
---
+++ PersonalFolder/ii   - Investigations/Lambda/Lambda1/Lambda1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Lambda1
+{
+
+    public delegate void Imprimir(string value);
+
+
+    public class Ejemplo
+    {
+        public void ImprimirPantalla(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public void EjemploDelegado()
+        {
+            Imprimir prueba1 = new Imprimir(ImprimirPantalla);
+            prueba1("esto es una prueba");
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Ejemplo ej1 = new Ejemplo();
+            ej1.EjemploDelegado();
+        }
+    }
+}
