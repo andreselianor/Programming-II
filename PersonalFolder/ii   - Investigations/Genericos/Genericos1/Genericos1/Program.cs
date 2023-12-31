a584948a621f722eb27229b7148842f0d653eb4a
andreselianor <113841548+andreselianor@users.noreply.github.com>
2023-12-31 18:33:13 +0100

    domingo 31DIC

---
+++ PersonalFolder/ii   - Investigations/Genericos/Genericos1/Genericos1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Genericos1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AlmacenGenericos<string> almacen1 = new AlmacenGenericos<string>(4);
+
+            almacen1.AddElement("Pepi");
+            almacen1.AddElement("Jordi");
+            almacen1.AddElement("Minerva");
+            almacen1.AddElement("Hipatia");
+
+            string nombre = almacen1.GetElement(2);
+            Console.WriteLine(nombre);
+
+            AlmacenGenericos<double> almacen2 = new AlmacenGenericos<double>(4);
+
+            almacen2.AddElement(0.5);
+            almacen2.AddElement(1.8);
+            almacen2.AddElement(22.4);
+            almacen2.AddElement(33.7);
+
+            double numero = almacen2.GetElement(2);
+            Console.WriteLine(nombre);
+        }
+    }
+}
