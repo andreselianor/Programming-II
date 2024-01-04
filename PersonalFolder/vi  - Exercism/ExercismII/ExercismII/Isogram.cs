dbc068b2b52382346f102c63c77935fbbefc6b4c
andreselianor <113841548+andreselianor@users.noreply.github.com>
2024-01-04 09:08:20 +0100

    Isogram 04ENE

---
+++ PersonalFolder/vi  - Exercism/ExercismII/ExercismII/Isogram.cs
@@ -0,0 +1,51 @@
+﻿namespace ExercismII
+{
+    public static class Isogram
+    {
+        public static bool IsIsogram(string word)
+        {
+            List<char> listResult = new List<char>();
+            word = word.ToLower();
+            listResult = AddCharToList(word);
+            listResult = RemoveSignFromList(listResult);
+            bool result = ContainsRepeatedChar(listResult);
+            return result;
+        }
+
+        public static List<char> AddCharToList(string word)
+        {
+            List<char> listResult = new List<char>();
+            for (int i = 0; i < word.Length; i++)
+            {
+                listResult.Add(word[i]);
+            }
+            return listResult;
+        }
+
+        public static List<char> RemoveSignFromList(List<char> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] < 97 || list[i] > 123)
+                {
+                    list.RemoveAt(i);
+                    i--;
+                }
+            }
+            return list;
+        }
+
+        public static bool ContainsRepeatedChar(List<char> list)
+        {
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    if (list[i] == list[j])
+                        return false;
+                }
+            }
+            return true;
+        }
+    }
+}
--- PersonalFolder/vi  - Exercism/ExercismII/ExercismII/Program.cs
+++ PersonalFolder/vi  - Exercism/ExercismII/ExercismII/Program.cs
@@ -4,14 +4,7 @@
     {
         static void Main(string[] args)
         {
-            string input = "he quick brown fox jumps over he lazy dog";
-            bool test1 = Pangram.IsPangram(input);
-
-            List<int> listResult = new List<int>();
-
-            int[] array = new int[10];
-            Range range = new Range(0, 10);
-            int value = range.Start;
+            
         }
     }
 }
