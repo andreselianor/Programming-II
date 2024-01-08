c348fedd42a682e33af06b2fae85933e2bef72f4
andreselianor <113841548+andreselianor@users.noreply.github.com>
2024-01-08 16:31:09 +0100

    update

---
+++ PROG/EV2/DAMLibTest/DAMLib/Stack.cs
@@ -0,0 +1,41 @@
+﻿namespace DAMLib
+{
+    public class Stack<T>
+    {
+        //private static int _capacity = 0;
+        private T[] _stackArray;
+        private int _count = 0;
+
+        public Stack(int number)
+        {
+            _stackArray = new T[number];
+        }
+
+        public void Push(T Element)
+        {
+            _stackArray[_count] = Element;
+            _count++;
+        }
+
+        public T Pop()
+        {
+            _count--;
+            return _stackArray[_count];
+        }
+
+        public T Top()
+        {
+            return _stackArray[_count];
+        }
+
+        public bool IsEmpty()
+        {
+            return _count < 0;
+        }
+
+        public int GetCount()
+        {
+            return _count;
+        }
+    }
+}
---
+++ PROG/EV2/DAMLibTest/DAMLibTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using DAMLib;
+
+namespace DAMLibTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> stack = new Stack<string>(10);
+            stack.Push("Efigenio");
+            stack.Push("Osvaldo");
+            stack.Top();
+            string result = stack.Pop();
+        }
+    }
+}
