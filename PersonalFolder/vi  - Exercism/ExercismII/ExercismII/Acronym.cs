5579355817c11b3704db55734c2b5b2db4462dda
andreselianor <113841548+andreselianor@users.noreply.github.com>
2024-01-08 11:47:10 +0100

    update

---
+++ PersonalFolder/vi  - Exercism/ExercismII/ExercismII/Acronym.cs
@@ -0,0 +1,36 @@
+﻿namespace ExercismII
+{
+    public class Acronym
+    {
+
+        public static string Abbreviate(string phrase)
+        {
+            string result = "";
+            for (int i = 0; i < phrase.Length; i++)
+            {
+                if (IsMayus(phrase[i]))
+                    result += phrase[i];
+                else if (i > 0 && IsSign(phrase[i - 1]))
+                    result += phrase[i];
+                
+            }
+
+            return result.ToUpper();
+        }
+
+        public static bool IsMayus(char letter)
+        {
+            return (64 < letter && letter < 91);
+        }
+
+        public static bool IsSign(char letter)
+        {
+            return (31 < letter && letter < 65);
+        }
+
+        public static bool IsSpace(char letter)
+        {
+            return letter == 32;
+        }
+    }
+}
