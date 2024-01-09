a74c1919e3c36a41bb5e7d106d6bb5b8959ce0aa
andreselianor <113841548+andreselianor@users.noreply.github.com>
2024-01-09 19:10:42 +0100

    update Clase 09ENE

---
+++ PROG/EV2/DAMLibTest/DAMLib/Queue.cs
@@ -0,0 +1,54 @@
+﻿namespace DAMLib
+{
+    public class Queue<T>
+    {
+        private T[] _queue;
+
+        public Queue()
+        {
+            _queue = new T[0];
+        }
+
+        public void Enqueue(T element)
+        {
+            int count = _queue.Length;
+            T[] _arrayResult = new T[count + 1];
+
+            for(int i = 0; i < count; i++)
+            {
+                _arrayResult[i] = _queue[i];
+            }
+
+            _arrayResult[count] = element;
+
+            _queue = _arrayResult;
+        }
+
+        public T Dequeue()
+        {
+            int count = _queue.Length;
+            T result = _queue[0];
+            T[] _arrayResult = new T[count - 1];
+
+            for(int i = 0; i < count; i++)
+            {
+                _arrayResult[i + 1] = _queue[i];
+            }
+
+            _queue = _arrayResult;
+
+            return result;
+        }
+
+        public void Clear()
+        {
+            T[] arrayResult = new T[0];
+            _queue = arrayResult;
+        }
+
+        public int Count => _queue.Length;
+        public T First => _queue[0];
+        public T Last => _queue[Count - 1];
+        public bool Empty => _queue.Length == 0;
+    }
+}
