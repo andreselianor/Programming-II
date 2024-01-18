0883e29132da7a02362162db2d572c095edd14ea
andreselianor <113841548+andreselianor@users.noreply.github.com>
2024-01-18 16:53:25 +0100

    clase 18 ENE JUEVES

---
+++ PersonalFolder/i    - 2aEVAL/Tuplas/Tuplas/AlternativeClass.cs
@@ -0,0 +1,22 @@
+﻿namespace Tuplas
+{
+    public class AlternativeClass
+    {
+        public static bool SolveEquation(double a, double b, double c, out double result1, out double result2)
+        {
+            result1 = double.NaN;
+            result2 = double.NaN;
+            if (a == 0.0)
+                return false;
+            double r = b * b - 4.0 * a * c;
+            if (r < 0.0)
+                return false;
+            double root = Math.Sqrt(r);
+            double denominator = 1.0 / 2.0 * a;
+            result1 = (-b + root) * denominator;
+            result2 = (-b - root) * denominator;
+
+            return true;
+        }
+    }
+}
---
+++ PersonalFolder/i    - 2aEVAL/Tuplas/Tuplas/TuplaClass.cs
@@ -0,0 +1,43 @@
+﻿namespace Tuplas
+{
+    public class TuplaClass
+    {
+        public static int GetResultsNumber(double a, double b, double c)
+        {
+            double discriminante = (b * b) - (4 * a * c);
+            if (discriminante > 0)
+                return 2;
+            if (discriminante == 0)
+                return 1;
+            return 0;
+        }
+
+        public static (double, double) GetEquationResult(double a, double b, double c)
+        {
+            double raiz = (b * b) - (4 * a * c);
+            double denominator = 2 * a;
+
+            if( denominator == 0)
+                return (double.NaN,double.NaN);
+
+            double result1 = (-b + Math.Sqrt(raiz)) / denominator;
+            double result2 = (-b - Math.Sqrt(raiz)) / denominator;
+
+            return (result1, result2);
+        }
+
+        public static double GetFirstResultEquation(double a, double b, double c)
+        {
+            var result = GetEquationResult(a, b, c);
+
+            return result.Item1;
+        }
+
+        public static double GetSecondtResultEquation(double a, double b, double c)
+        {
+            var result = GetEquationResult(a, b, c);
+
+            return result.Item2;
+        }
+    }
+}
