feb4d4d58bdb469797afbeba07a8327efea82820
andreselianor <113841548+andreselianor@users.noreply.github.com>
2024-01-20 19:56:35 +0100

    update 20 ENESabado

---
+++ PersonalFolder/ii   - Investigations/JsonBuilder/ThomasRiejkaard/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ThomasRiejkaard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TagBuilder builder = new TagBuilder();
+
+            int number = 50;
+            for(int i = 0; i < number; i++)
+            {
+                builder.CallDelegate();
+                builder.Serialize();
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PersonalFolder/ii   - Investigations/JsonBuilder/ThomasRiejkaard/TagBuilder.cs
@@ -0,0 +1,81 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace ThomasRiejkaard
+{
+    public class TagBuilder
+    {
+        private string _tag = "";
+        private StringBuilder _builder = new StringBuilder();
+        private Random _random = new Random();
+        public delegate void DelegateAdd();
+
+
+        public void AddCharacter()
+        {
+            char letter = (char)_random.Next(97, 123);
+            _builder.AppendFormat("" + letter);
+        }
+        public void AddMajorCharacter()
+        {
+            char letter = (char)_random.Next(65, 91);
+            _builder.AppendFormat("" + letter);
+        }
+        public void AddSimbol()
+        {
+            char letter = (char)_random.Next(34, 48);
+            _builder.AppendFormat("" + letter);
+        }
+        public void AddNumber()
+        {
+            char letter = (char)_random.Next(48, 58);
+            _builder.AppendFormat("" + letter);
+        }
+        public void AddAlter()
+        {
+            char letter = (char)_random.Next(166, 180);
+            _builder.AppendFormat("" + letter);
+        }
+
+        public void WriteDownTag()
+        {
+            int number = 2;
+            for (int i = 0; i < number; i++)
+            {
+                AddMajorCharacter();
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                AddNumber();
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                AddSimbol();
+            }
+            for (int i = 0; i < number; i++)
+            {
+                AddCharacter();
+            }
+        }
+
+
+        public void CallDelegate()
+        {
+            DelegateAdd delegado = new DelegateAdd(WriteDownTag);
+            delegado();
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(_builder);
+        }
+
+        public void Serialize()
+        {
+            string fileName = "../../../JsonText.Json";
+            string jsonString = JsonSerializer.Serialize(_builder.ToString());
+            //File.WriteAllText(fileName, jsonString);
+            File.AppendAllText(fileName, jsonString);
+        }
+    }
+}
