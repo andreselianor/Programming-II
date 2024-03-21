96e529fd502f60984d7586ef518f2830cd47faa1
andreselianor <113841548+andreselianor@users.noreply.github.com>
2024-03-21 16:50:39 +0100

    clase jueves

---
+++ PROG/EV3/RemoveTest/RemoveTest/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace RemoveTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Test t1 = CrearPrueba())
+            {
+                t1.SayHello();
+            }
+                
+            //t1.Dispose();
+        }
+
+
+        public static Test CrearPrueba()
+        {
+            return new Test();
+        }
+    }
+
+    public class Test : IDisposable
+    {
+        public void Dispose()
+        {
+            Console.WriteLine("Dispose");
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+
+    }
+}
