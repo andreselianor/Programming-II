6c5c58aed80fd1b20701bf5efbb48b3fc5089347
andreselianor <113841548+andreselianor@users.noreply.github.com>
2024-05-06 16:53:50 +0200

    UPD

--- PROG/EV3/ConnectingDatabase/ConnectingDatabase/Program.cs
+++ PROG/EV3/ConnectingDatabase/ConnectingDatabase/Program.cs
@@ -6,16 +6,39 @@ namespace ConnectingDatabase
     {
         static void Main(string[] args)
         {
-            TestSQL test = new TestSQL();
-            test.Test();
+            TestConnection();
         }
+        public const string conectionString = "Data Source=192.168.56.101,1433;Database=STUDENT;User ID=sa;Password=SqlServer123;";
+        //192.168.56.101
+        //nombre de la base de datos, STUDENTS
+        //userID = sa
+        //PASS = SqlServer123
+
+        public static void TestConnection()
+        {
+            
+            SqlConnection connection = new SqlConnection(conectionString);
+
+            connection.Open();
+
+            connection.Dispose();
+            /*
+            using (SqlConnection connection = new SqlConnection(conectionString))
+            {
+                SqlCommand command = new SqlCommand("SELECT * FROM employees", connection);
+                command.Connection.Open();
+                command.ExecuteNonQuery();
+            }
+            */
+        }
+
     }
+
 
     public class TestSQL
     {
         public void Test()
         {
-
             using (SqlConnection connection = new SqlConnection("../../../sampleDB/sampleDatabase.sql"))
             {
                 SqlCommand command = new SqlCommand("SELECT * FROM employees", connection);
