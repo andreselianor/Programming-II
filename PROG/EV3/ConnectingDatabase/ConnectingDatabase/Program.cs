ad58b3ba882611ee22c793ca2a140a534fc29261
andreselianor <113841548+andreselianor@users.noreply.github.com>
2024-05-09 16:47:36 +0200

    UPD

--- PROG/EV3/ConnectingDatabase/ConnectingDatabase/Program.cs
+++ PROG/EV3/ConnectingDatabase/ConnectingDatabase/Program.cs
@@ -6,44 +6,80 @@ namespace ConnectingDatabase
     {
         static void Main(string[] args)
         {
-            TestConnection();
+            TestConnection4();
         }
-        public const string conectionString = "Data Source=192.168.56.101,1433;Database=STUDENT;User ID=sa;Password=SqlServer123;";
+
+        public const string conectionString = "Data Source=192.168.56.101,1433;Database=SUPERTIENDA;User ID=sa;Password=SqlServer123;";
+
         //192.168.56.101
         //nombre de la base de datos, STUDENTS
         //userID = sa
         //PASS = SqlServer123
 
-        public static void TestConnection()
+        public static void TestConnection1()
         {
-            
             SqlConnection connection = new SqlConnection(conectionString);
-
             connection.Open();
-
             connection.Dispose();
-            /*
+        }
+
+        public static void TestConnection2()
+        {
             using (SqlConnection connection = new SqlConnection(conectionString))
             {
-                SqlCommand command = new SqlCommand("SELECT * FROM employees", connection);
+                SqlCommand command = new SqlCommand("SELECT * FROM CLASEDAM", connection);
                 command.Connection.Open();
                 command.ExecuteNonQuery();
             }
-            */
         }
 
-    }
-
-
-    public class TestSQL
-    {
-        public void Test()
+        public static void TestConnectionCHATGPT()
         {
-            using (SqlConnection connection = new SqlConnection("../../../sampleDB/sampleDatabase.sql"))
+            using (SqlConnection connection = new SqlConnection(conectionString))
             {
-                SqlCommand command = new SqlCommand("SELECT * FROM employees", connection);
-                command.Connection.Open();
-                command.ExecuteNonQuery();
+                connection.Open();
+                string query = "SELECT * FROM CLASEDAM WHERE NAME = 'andres'";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine(reader["NAME"].ToString());
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void TestConnection3()
+        {
+            using (SqlConnection connection = new SqlConnection(conectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT * FROM CLASEDAM", connection);
+
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader[1].ToString());
+                }
+            }
+        }
+
+        public static void TestConnection4()
+        {
+            using (SqlConnection connection = new SqlConnection(conectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT * FROM PRODUCTOS", connection);
+
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader[0].ToString() + ", " + reader[1].ToString() + ", " + reader[2].ToString() + ", " + reader[3].ToString() + ", " + reader[4].ToString());
+                }
             }
         }
     }
