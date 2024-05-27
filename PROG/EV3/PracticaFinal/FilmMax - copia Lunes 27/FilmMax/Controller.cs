dfa66d2fb6088c1d5bf4575d75846c462a629f59
andreselianor <113841548+andreselianor@users.noreply.github.com>
2024-05-27 16:51:19 +0200

    UPD

---
+++ PROG/EV3/PracticaFinal/FilmMax - copia Lunes 27/FilmMax/Controller.cs
@@ -0,0 +1,17 @@
+﻿using LibraryFilmMax;
+using MongoDB.Driver;
+
+namespace FilmMax
+{
+    public class Controller : ICore
+    {
+        private IDatabase _database = new Database();
+
+        public IDatabase Database => _database;
+
+        public void CreateConnection()
+        {
+            _database.ConnectMongoDB();
+        }
+    }
+}
---
+++ PROG/EV3/PracticaFinal/FilmMax - copia Lunes 27/LibraryFilmMax/IDatabase.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace LibraryFilmMax
+{
+    public interface IDatabase
+    {
+        void ConnectMongoDB();
+        IMongoDatabase DatabaseMongo { get; }
+        IMongoCollection<User> UsersCollection { get; }
+
+
+        ObjectId CreateUser(User user);
+        User ReadUser(User user);
+        void UpdateUser(User user, string field, string value);
+        void DeleteUser(User user);
+
+
+        ObjectId CreateMovie(Movie movie);
+        void ReadMovie(Movie movie);
+        void UpdateMovie(Movie movie, string field, string value);
+        void DeleteMovie(Movie movie);
+
+
+        
+        List<User> GetAllUsers();
+        int GetIndexOf(User user);
+        User? GetUserWithLoginName(string loginName);
+        User? GetUserAtIndex(int index);
+        bool IsValidUser(User user);
+    }
+}
